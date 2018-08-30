07d15e26583b38faa96f8b2361462a4294d9b8b0
DESKTOP-G7GTOD0\Dugongs <agithapramesti.s@gmail.com>
2018-08-30 19:32:27 +0700

    change pass

--- src/main/java/com/example/mail/model/MyContants.java
+++ src/main/java/com/example/mail/model/MyContants.java
@@ -3,7 +3,7 @@ package com.example.mail.model;
 public class MyContants {
 
     public static final String myEmail = "gegetest366@gmail.com";
-    public static final String myPassword = "miaoBell366";
+    public static final String myPassword = "**";
     public static final String brendaEmail = "brenda.natasha@si.ukdw.ac.id";
     public static final String koJason = "jason.alexander@gdn-commerce.com";
     public static final String friendEmail = "agithapramesti.s.m@gmail.com";
