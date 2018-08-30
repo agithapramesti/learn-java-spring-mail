d91561e2f13bc758175866c9b2bcb40b50a21cf6
DESKTOP-G7GTOD0\Dugongs <agithapramesti.s@gmail.com>
2018-08-30 11:21:25 +0700

    initial commit

---
+++ src/main/java/com/example/mail/model/MyContants.java
@@ -0,0 +1,9 @@
+package com.example.mail.model;
+
+public class MyContants {
+
+    public static final String myEmail = "gegetest366@gmail.com";
+    public static final String myPassword = "miaoBell366";
+
+    public static final String friendEmail = "agithapramesti.s.m@gmail.com";
+}
